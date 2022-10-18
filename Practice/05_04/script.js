5936d1eb507c6982529d49e143034adb149792fe
thor4 <6412103+thor4@users.noreply.github.com>
2022-10-17 21:50:49 -0400

    update working through 0508 practice

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,7 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+const mainty = document.querySelector(".sitefooter");
+
+mainty.style.background = "red";
